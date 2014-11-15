8f7da40cae2cb80b10b7f21513964a330a6a218a
Andrey Kislyuk <kislyuk@gmail.com>
2014-11-15 14:54:54 -0800

    WIP

---
+++ src/python/test/mock_api/api.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+from __future__ import print_function, unicode_literals
+
+import os, random, hashlib
+from flask import Flask, request, jsonify
+app = Flask(__name__)
+random.seed(1)
+
+payload = b""
+for i in range(4):
+    r = random.getrandbits(1024*1024*1024)
+    payload += r.to_bytes((r.bit_length() // 8) + 1, 'little')
+
+file_desc = {
+    "id": "file-0123456789ABCDEF01234567",
+    "class": "file",
+    "name": "test",
+    "state": "closed",
+    "size": len(payload),
+    "md5": hashlib.md5(payload).hexdigest()
+}
+
+@app.route("/system/findDataObjects", methods=["POST"])
+def find_data_objects():
+    results=[]
+    results.append(dict(project=request.json["scope"]["project"],
+                        id=file_desc["id"],
+                        describe=file_desc))
+    return jsonify(dict(results=results, next=None))
+
+@app.route("/<subject>/listFolder", methods=["POST"])
+def list_folder(subject):
+    folders=[]
+    objects=[]
+    return jsonify(dict(folders=folders, objects=objects))
+
+@app.route("/<subject>/describe", methods=["POST"])
+def describe(subject):
+    if subject.startswith("project-"):
+        return jsonify(dict(folders=[]))
+    elif subject.startswith("file-"):
+        return jsonify(file_desc)
+
+@app.route("/file-<id>/download", methods=["POST"])
+def download(id):
+    url = request.url_root + "F/D"
+    return jsonify(dict(url=url))
+
+@app.route("/F/D", methods=["GET"])
+def serve_download():
+    start, stop = (int(x) for x in request.headers["range"].split("=")[1].split("-"))
+    #print("SERVING", request.url, start, stop, request.headers)
+    return payload[start:stop+1]
+
+if __name__ == "__main__":
+    app.run(debug=True, use_reloader=False)
---
+++ src/python/test/mock_api/run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+./api.py > /dev/null 2>&1 &
+MOCK_SERVER_PID=$!
+
+cleanup() {
+    kill $MOCK_SERVER_PID
+}
+
+trap cleanup EXIT
+
+export DX_APISERVER_PROTOCOL=http
+export DX_APISERVER_HOST=localhost
+export DX_APISERVER_PORT=5000
+#export _DX_DEBUG=1
+
+for i in {1..1024}; do
+    wire_md5=$(dx download test --output - 2>/dev/null | md5sum | cut -f 1 -d " ")
+    desc_md5=$(dx api file-test describe | jq --raw-output .md5)
+    echo $i $wire_md5 $desc_md5
+    if ! [[ $wire_md5 == $desc_md5 ]]; then
+        exit 1
+    fi
+done
