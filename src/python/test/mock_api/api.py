a5e9c4efb8b9330f72c8399cbdc1409a52750697
Andrey Kislyuk <kislyuk@gmail.com>
2014-11-16 09:54:12 -0800

    WIP

--- src/python/test/mock_api/api.py
+++ src/python/test/mock_api/api.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python2.7
 
 from __future__ import print_function, unicode_literals
 
-import os, sys, random, hashlib, argparse, io
+import os, sys, random, hashlib, argparse, io, struct
 from flask import Flask, request, jsonify
 
 parser = argparse.ArgumentParser(description=__doc__)
@@ -24,7 +24,7 @@ def set_payload():
     payload = io.BytesIO()
     for i in range(4):
         r = random.getrandbits(1024*1024*1024)
-        payload.write(r.to_bytes((r.bit_length() // 8) + 1, 'little'))
+        payload.write(long_to_bytes(r))
     app.payload = payload.getvalue()
     file_desc["md5"] = hashlib.md5(app.payload).hexdigest()
     file_desc["size"] = len(app.payload)
@@ -65,6 +65,24 @@ def serve_download():
     start, stop = (int(x) for x in request.headers["range"].split("=")[1].split("-"))
     #print("SERVING", request.url, start, stop, request.headers)
     return app.payload[start:stop+1]
+
+def long_to_bytes(n, blocksize=0):
+    s = b''
+    pack = struct.pack
+    while n > 0:
+        s = pack(b'>I', n & 0xffffffffL) + s
+        n = n >> 32
+    for i in range(len(s)):
+        if s[i] != b'\000'[0]:
+            break
+    else:
+        s = b'\000'
+        i = 0
+    s = s[i:]
+    if blocksize > 0 and len(s) % blocksize:
+        s = (blocksize - len(s) % blocksize) * b'\000' + s
+    return s
+
 
 if __name__ == "__main__":
     app.run(debug=True, use_reloader=False, port=args.port)
