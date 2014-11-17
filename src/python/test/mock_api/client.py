b34b5a5455816dcc1cb2e64d8424ceeb9840c020
Andrey Kislyuk <kislyuk@gmail.com>
2014-11-17 12:03:26 -0800

    WIP

--- src/python/test/mock_api/client.py
+++ src/python/test/mock_api/client.py
@@ -18,7 +18,22 @@ oneMB = 1048576
 rndbufsz = oneMB*10
 rndbuf = os.urandom(rndbufsz/2).encode('hex')
 
+def get_pseudo_random_MB(seed):
+  r = random.Random()
+  b = bytearray()
+  r.seed(seed)
+  byte4min=-(2**31)
+  byte4max= (2**31) - 1
+  # 2^18 4-byte random numbers for a total of 1MB
+  # getting 4 random bytes at a time instead of 1 byte to make things faster
+  for i in xrange(2**18):
+    b.extend(struct.pack('>i',r.randint(byte4min, byte4max)))
+  return b
+
 for i in range(8196):
+    t=time.time()
+    ics = hashlib.md5(get_pseudo_random_MB(str(i))).hexdigest()
+    mb = os.urandom(oneMB)
     dxpy.DXHTTPRequest("/system/setPayload", {})
     file_desc = dxpy.describe("file-0123456789ABCDEF01234567")
     fh = dxpy.open_dxfile("file-0123456789ABCDEF01234567")
